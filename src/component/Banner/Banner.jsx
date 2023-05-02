c2795ee4f65fa0f313359e0ebcc22343bec85b8e
Mridul1x <mridulhasannn@gmail.com>
2023-05-03 05:41:52 +0600

    Banner Done

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import "./Banner.css";
+import food1 from "../../assets/food1 (1).png";
+import food2 from "../../assets/food1 (2).png";
+import food3 from "../../assets/food1 (3).png";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner Coming Soon</h2>
-
-      <button className="btn btn-active btn-primary">Button</button>
+    <div className="h-screen  container mx-auto banner flex flex-col lg:flex-row justify-center items-center px-5">
+      <div className="text-center lg:text-start">
+        <h2 className="text-3xl lg:text-6xl text-stone-300 font-bold">
+          Welcome to World Class Chef !
+        </h2>
+        <p className="text-base font-medium my-3 text-stone-400">
+          Welcome to our chef-inspired world of culinary delight! Let us take
+          you on a journey of <br /> flavor and texture with our expertly
+          crafted dishes
+        </p>
+        <Link to="/about">
+          <button className="btn btn-outline rounded-full text-stone-300">
+            About Us
+          </button>
+        </Link>
+      </div>
+      <Carousel className="lg:w-3/5 mx-auto mt-5 lg:mt-0">
+        <div>
+          <img src={food1} />
+          <p className="legend">Lasagne</p>
+        </div>
+        <div>
+          <img src={food2} />
+          <p className="legend">Margherita Pizza</p>
+        </div>
+        <div>
+          <img src={food3} />
+          <p className="legend">Spaghetti Carbonara</p>
+        </div>
+      </Carousel>
     </div>
   );
 };
--- src/shared/NavigationBar.jsx
+++ src/shared/NavigationBar.jsx
@@ -4,7 +4,7 @@ import { FaBeer, FaUserCircle } from "react-icons/fa";
 
 const NavigationBar = () => {
   return (
-    <div className="navbar container mx-auto bg-indigo-700">
+    <div className="navbar container mx-auto bg-slate-950 ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -28,49 +28,39 @@ const NavigationBar = () => {
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Item 1</a>
+              <Link to="/">Home</Link>
             </li>
             <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <Link to="/blog">Blog</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link to="/about">About</Link>
             </li>
+            <Link
+              to="/"
+              className="normal-case text-center font-semibold border border-slate-500  rounded-lg "
+            >
+              The Art of Italian Cooking
+            </Link>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-white text-xl">
+        <Link
+          to="/"
+          className="btn btn-ghost normal-case text-white text-xl hidden lg:block pt-2 "
+        >
           The Art of Italian Cooking
-        </a>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 text-white">
           <li>
             <Link to="/">Home</Link>
           </li>
           <li tabIndex={0}>
-            <Link to="/blog">blog</Link>
+            <Link to="/blog">Blog</Link>
           </li>
           <li>
-            <Link to="/">Blog</Link>
+            <Link to="/">About</Link>
           </li>
         </ul>
       </div>
@@ -81,7 +71,9 @@ const NavigationBar = () => {
             style={{ fontSize: "2rem" }}
           ></FaUserCircle>
         </div>
-        <a className="btn btn-secondary">Login</a>
+        <Link to="/login" className="btn btn-outline  text-white font-semibold">
+          Login
+        </Link>
       </div>
     </div>
   );
